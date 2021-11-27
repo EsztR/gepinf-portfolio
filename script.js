359adbdececec0b6030353bbee61bafd30089e38
EsztR <95129338+EsztR@users.noreply.github.com>
2021-11-27 13:47:57 +0100

    Add webpage files

---
+++ script.js
@@ -0,0 +1,13 @@
+navigacio.innerHTML = `
+    <a href="index.html">Kezdőlap</a>
+    &emsp;
+    <a href="bemutatkozas.html">Bemutatkozás</a>
+    &emsp;
+    <a href="kepek.html">Képek</a>
+    &emsp;
+    <a href="hogyankeszult.html">Hogyan készült</a>
+`;
+
+lablec.innerHTML = `
+    Elérhetőség: sinkovicseszter@gmail.com
+`;
